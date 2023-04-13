b22ae4fc2a53d012dc51f82696c8d44b2cc3dad7
MarkLangren <markdreamer@163.com>
2023-04-14 06:56:15 +0800

    keyun

---
+++ group9-csc2063-client/src/main/java/projects/projectnumber.java
@@ -0,0 +1,11 @@
+package projects;
+
+public class projectnumber {
+	private String projectnumber;
+	
+	projectnumber(){}
+	
+	public projectnumber(String projectN) {
+		this.projectnumber = projectN;
+	}
+}
